b3a4e711a8c98f7c30ef0f80208bf17af4538fa7
P0mid00r <sergebelousov2018@gmail.com>
2026-02-17 18:40:33 +0700

    Изменение версии приложения на 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "ru.pomidorka.weatherapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
